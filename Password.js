dd2e568948421f70067010809f7d199c3bccee34
Alexander <alexandermcclement9@gmail.com>
2019-12-11 20:12:17 -0800

    Special Character True or False

--- Password.js
+++ Password.js
@@ -4,7 +4,11 @@
 var userPasswordLength = prompt('enter a number between 8 and 128')
 
 console.log(userPasswordLength)
-
 for (var i = userPasswordLength; userPasswordLength < 8 || userPasswordLength > 128; userPasswordLength = prompt('enter a number between 8 and 128')) {
     alert('not smaller then 8 or greater then number 128')
 }
+
+var wantSpecialCharacter = confirm('Do you want a special Character?')
+
+console.log(wantSpecialCharacter)
+
