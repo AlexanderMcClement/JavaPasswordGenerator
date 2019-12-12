dd4eabe6c88bc04704194dc5bb96051491c1b896
Alexander <alexandermcclement9@gmail.com>
2019-12-11 19:34:42 -0800

    initial commit and greater than 8 number

---
+++ Password.js
@@ -0,0 +1,12 @@
+
+
+
+var userPasswordLength = prompt('enter a number between 8 and 128')
+
+console.log(userPasswordLength)
+
+if (userPasswordLength < 8) {
+    alert('not great then number 8')
+
+
+}
