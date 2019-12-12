d8f21da1d1a727549106c333733d7023855a468f
Alexander <alexandermcclement9@gmail.com>
2019-12-11 19:53:36 -0800

    Got correct numbers from user

--- Password.js
+++ Password.js
@@ -5,14 +5,6 @@ var userPasswordLength = prompt('enter a number between 8 and 128')
 
 console.log(userPasswordLength)
 
-if (userPasswordLength < 8) {
-    alert('not great then number 8')
-
-
-}
-
-if (userPasswordLength > 128) {
-    alert('not small then number 128')
-
-
+for (var i = userPasswordLength; userPasswordLength < 8 || userPasswordLength > 128; userPasswordLength = prompt('enter a number between 8 and 128')) {
+    alert('not smaller then 8 or greater then number 128')
 }
